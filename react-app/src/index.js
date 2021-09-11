58188bfac17c6c3203e655ac4431bfb8252f3637
Nilashan Arul <nilashan.arul@gmail.com>
2021-09-11 11:56:56 +0200

    install bootstrap

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 const element = <h1>Hello World</h1>;
 ReactDOM.render(element, document.getElementById("root"));
