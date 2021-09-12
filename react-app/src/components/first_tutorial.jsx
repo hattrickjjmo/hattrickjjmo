d3f668fe3b452d5fca72c4765ca7898780ad8ebe
Nilashan Arul <nilashan.arul@gmail.com>
2021-09-12 10:36:27 +0200

    basic react and bootstrap

---
+++ react-app/src/components/first_tutorial.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class tutorial1 extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning text-dark" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default tutorial1;
